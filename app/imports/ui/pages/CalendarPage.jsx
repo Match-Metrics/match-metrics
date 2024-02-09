c429986dd9b87c01b88c63db07ef0f7738eb7778
caciems <caciems@hawaii.edu>
2024-02-08 21:07:57 -1000

    Set up calendar

--- app/imports/ui/pages/CalendarPage.jsx
+++ app/imports/ui/pages/CalendarPage.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-// import FullCalendar from '@fullcalendar/react';
-// import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import { Container } from 'react-bootstrap';
 
+// const handleDateClick = (arg) => {
+// alert(arg.dateStr);
+// };
+// dateClick={handleDateClick}
+
+// Editable: You can move events
 const CalendarPage = () => (
-  <Container className="py-3">
-    <p>Calendar</p>
-  </Container>
+  <div>
+    <Container>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        intialView="dayGridMonth"
+        height="90vh"
+        events={[
+          { title: 'event 1', date: '2024-02-09' },
+          { title: 'event 2', date: '2024-02-14' },
+        ]}
+        selectable="true"
+      />
+    </Container>
+  </div>
 );
 
 export default CalendarPage;
