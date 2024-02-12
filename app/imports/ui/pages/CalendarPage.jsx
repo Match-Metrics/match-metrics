48dfc6dc823b11775c8170a029e08e2ef4ca54a1
caciems <caciems@hawaii.edu>
2024-02-11 21:52:54 -1000

    Added "Add Event" button

--- app/imports/ui/pages/CalendarPage.jsx
+++ app/imports/ui/pages/CalendarPage.jsx
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { Container } from 'react-bootstrap';
 
-// const handleDateClick = (arg) => {
-// alert(arg.dateStr);
-// };
-// dateClick={handleDateClick}
+const CalendarPage = () => {
+  const calendarRef = useRef(null);
 
-// Editable: You can move events
-const CalendarPage = () => (
-  <div>
-    <Container>
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        intialView="dayGridMonth"
-        height="90vh"
-        events={[
-          { title: 'event 1', date: '2024-02-09' },
-          { title: 'event 2', date: '2024-02-14' },
-        ]}
-        selectable="true"
-      />
-    </Container>
-  </div>
-);
+  const handleAddEvent = () => {
+    const dateStr = prompt('Enter a date in YYYY-MM-DD');
+    const date = new Date(`${dateStr}T00:00:00`);
+
+    if (!Number.isNaN(date.valueOf())) {
+      const calendarApi = calendarRef.current.getApi();
+
+      calendarApi.addEvent({
+        title: 'New Event',
+        start: date,
+        allDay: true,
+      });
+      alert('Adding event to calendar...');
+    } else {
+      alert('Invalid date');
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <FullCalendar
+          ref={calendarRef}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          intialView="dayGridMonth"
+          height="90vh"
+          selectable="true"
+          editable="true"
+          headerToolbar={{
+            center: 'addEventButton',
+          }}
+          customButtons={{
+            addEventButton: {
+              text: 'Add event',
+              click: handleAddEvent,
+            },
+          }}
+        />
+      </Container>
+    </div>
+  );
+};
 
 export default CalendarPage;
